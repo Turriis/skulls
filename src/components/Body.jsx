42073547f8894a146626cf28a1a5fef0fbd3ce00
Turris <arturocecena@hotmail.com>
2024-09-23 08:14:32 -0600

    First commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,14 @@
+import "../App.css"
+
+const Body = () => {
+    return (
+      <body>
+        <div>as</div>
+        <img src="skeleton-berserk.gif" alt="skeleton-berserk" />
+      </body>
+    );
+
+
+}
+
+export default Body;
